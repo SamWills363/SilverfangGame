3b41476b22c02ee812a220b93eec2493dbc5bda7
SamWills363 <lilpunkboy81@gmail.com>
2024-12-14 08:49:02 +0700

    Fix (hopefully)

--- Assets/SCRIPTS/AudioVolume.cs
+++ Assets/SCRIPTS/AudioVolume.cs
@@ -8,46 +8,36 @@ public class AudioVolume : MonoBehaviour
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 
-    public void Start(){
+    public void start(){
         if(PlayerPrefs.HasKey("musicVolume")){
             LoadVolumeMusic();
         }
-        if(PlayerPrefs.HasKey("sfxVolume")){
+        if(PlayerPrefs.HasKey("sfxvolume")){
             LoadVolumeSFX();
         }
     }
 
 
-    public void SetMusicVolume()
-    {
-        float musicVolume = Mathf.Clamp(musicSlider.value, 0.01f, 1); // Ensure non-zero values
-        float musicDB = Mathf.Log10(musicVolume) * 20;
-        audioMixer.SetFloat("Music", musicDB);
-        PlayerPrefs.SetFloat("musicVolume", musicDB);
-        PlayerPrefs.SetFloat("musicSlider", musicVolume);
+    public void SetMusicVolume(){
+        float musicvolume = musicSlider.value;
+        float mscvlm = Mathf.Log10(musicvolume)*20;
+        audioMixer.SetFloat("Music", mscvlm);
+        PlayerPrefs.SetFloat("musicVolume", mscvlm);
+        PlayerPrefs.Save();
+    }
+    public void SetSFXVolume(){
+        float sfxvolume = sfxSlider.value;
+        float sfxvlm = Mathf.Log10(sfxvolume)*20;
+        audioMixer.SetFloat("SFX", sfxvlm);
+        PlayerPrefs.SetFloat("sfxvolume", sfxvlm);
         PlayerPrefs.Save();
     }
 
-    public void SetSFXVolume()
-    {
-        float sfxVolume = Mathf.Clamp(sfxSlider.value, 0.01f, 1); // Ensure non-zero values
-        float sfxDB = Mathf.Log10(sfxVolume) * 20;
-        audioMixer.SetFloat("SFX", sfxDB);
-        PlayerPrefs.SetFloat("sfxVolume", sfxDB); // Corrected key
-        PlayerPrefs.SetFloat("sfxSlider", sfxVolume);
-        PlayerPrefs.Save();
-    }
-    private void LoadVolumeMusic (){
-        
-        audioMixer.SetFloat("Music", PlayerPrefs.GetFloat("musicVolume"));
-
-        musicSlider.value = PlayerPrefs.GetFloat("musicSlider");
+    private void LoadVolumeMusic(){
+        audioMixer.SetFloat("Music", PlayerPrefs.GetFloat("musicVolume")); 
     }
 
     private void LoadVolumeSFX(){
-        
-        audioMixer.SetFloat("SFX", PlayerPrefs.GetFloat("sfxVolume"));
-
-        sfxSlider.value = PlayerPrefs.GetFloat("sfxSlider");
+        audioMixer.SetFloat("Music", PlayerPrefs.GetFloat("musicVolume"));
     }
 }
