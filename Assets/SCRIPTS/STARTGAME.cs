e0e361efda958697f68cfae1b935bd8f8caf4e1a
SamWills363 <lilpunkboy81@gmail.com>
2024-12-19 14:41:31 +0700

    Synchronizing

--- Assets/SCRIPTS/STARTGAME.cs
+++ Assets/SCRIPTS/STARTGAME.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class STARTGAME : MonoBehaviour
 {
     public GameObject startMenu;
+    public GameObject menuCamera;
+    public GameObject menuBackGround;
     public void StartGame()
     {
         Time.timeScale = 1f;
         startMenu.SetActive(false);
+        menuCamera.SetActive(false);
+        menuBackGround.SetActive(false);
     }
 }
